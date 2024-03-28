b5fd63170afce0bd24d645d017c64c937a057195
Flx232 <98140466+Flx232@users.noreply.github.com>
2024-03-28 01:06:49 -0400

    Revert "safeguard push"

--- api/model/car.py
+++ api/model/car.py
@@ -1,11 +1,11 @@
 class Car:
-    def __init__(self, make=None, model=None, year=None, color=None, type=None, mpg=None, price=0, available=1, image=None):
+    def __init__(self, car_id=None, make=None, model=None, year=None, color=None, type=None, mpg=None, available=1, image=None):
+        self.car_id = car_id
         self.make = make
         self.model = model
         self.year = year
         self.color = color
         self.type = type
         self.mpg = mpg
-        self.price = price
         self.available = available
         self.image = image
